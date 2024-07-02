3b82b6ac642c081c0d3f52ddeda2e3433b8561fb
Gaponov Stanislav <rashdor@gmail.com>
2024-07-02 14:53:22 +0200

    3. Longest Substring Without Repeating Characters
    https://leetcode.com/problems/longest-substring-without-repeating-characters/

--- app/Tasks/LongestSubstringWithoutRepeatingCharactersClass.php
+++ app/Tasks/LongestSubstringWithoutRepeatingCharactersClass.php
@@ -4,6 +4,36 @@ declare(strict_types = 1);
 
 namespace App\Tasks;
 
+/**
+ * Given a string s, find the length of the longest
+ * substring
+ * without repeating characters.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: s = "abcabcbb"
+ * Output: 3
+ * Explanation: The answer is "abc", with the length of 3.
+ * Example 2:
+ *
+ * Input: s = "bbbbb"
+ * Output: 1
+ * Explanation: The answer is "b", with the length of 1.
+ * Example 3:
+ *
+ * Input: s = "pwwkew"
+ * Output: 3
+ * Explanation: The answer is "wke", with the length of 3.
+ * Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+ *
+ *
+ * Constraints:
+ *
+ * 0 <= s.length <= 5 * 104
+ * s consists of English letters, digits, symbols and spaces.
+ */
 final class LongestSubstringWithoutRepeatingCharactersClass
 {
 
@@ -26,14 +56,20 @@ final class LongestSubstringWithoutRepeatingCharactersClass
 
         for ($i = 0; $i <= $strLen; $i++) {
 
-            $existsIndex = $i == $strLen ? 0 : array_search($string[$i], $lettersArray);
+            $existsIndex = $i != $strLen
+                ? array_search($string[$i], $lettersArray)
+                : true;
+
+            // already exists
             if ($existsIndex !== false) {
                 if (count($lettersArray) > $count) {
                     $count = count($lettersArray);
                 }
 
+                // no need to continue, coz word has ended
                 if ($i == $strLen) break;
 
+                // slice word from unique letter
                 $lettersArray = array_slice($lettersArray, $existsIndex + 1);
             }
             $lettersArray[] = $string[$i];
